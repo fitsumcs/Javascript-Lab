24248c1186b87b0209d22b922d508d4284b5e5ae
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-09 12:34:07 +0300

    Checking Age for Voting eligiblity and parsing the input age to Number

--- Lesson 01 [Lab 04]/assets/js/app.js
+++ Lesson 01 [Lab 04]/assets/js/app.js
@@ -4,6 +4,10 @@ var firstName;
 var lastName;
 var age;
 var job;
+//Age var for Holding Integer Value  
+let tempAge;
+//Eligible to vote 
+let isEligibleToVote;
 
 
 // Receive the values from input 
@@ -13,7 +17,21 @@ job = prompt("What is Your Profession ?")
 age = prompt("Enter Your Age");
 
 
+tempAge = parseInt(age);
+
+
+// check Eligible 
+if (tempAge >= 18) {
+    isEligibleToVote = true;
+} else {
+    isEligibleToVote = false;
+}
+
+
+
+
 console.log("Here is your Profile ")
 console.log("Full Name: " + firstName + " " + lastName);
 console.log("Profession : " + job);
 console.log("Age : " + age + " " + "years old");
+console.log("Is Eligible to Vote : " + isEligibleToVote);
