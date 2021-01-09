eacaf6dcf638acf950e1bf659cf13191bf37afda
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-09 12:19:16 +0300

    Receive the values from input dialog

--- Lesson 01 [Lab 04]/assets/js/app.js
+++ Lesson 01 [Lab 04]/assets/js/app.js
@@ -1,6 +1,13 @@
 // Declaring variables 
-// You can check this Variables are Available or not under the windows object 
+// You can check this Variables are Available or not under the window object 
 var firstName;
 var lastName;
 var age;
 var job;
+
+
+// Receive the values from input 
+firstName = prompt("Enter Your First Name");
+lastName = prompt("Enter Your Last Name");
+job = prompt("What is Your Profession ?")
+age = prompt("Enter Your Age");
