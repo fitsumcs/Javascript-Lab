6a1235d5a4d7e66ffd0bf6830d9acd98878f33ee
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-11 17:56:08 +0300

    Adding Event Listener for form , clearBtn and filter text input

--- Lesson 02 [Lab 05]/Finished/assets/js/app.js
+++ Lesson 02 [Lab 05]/Finished/assets/js/app.js
@@ -4,3 +4,13 @@ const form = document.querySelector('#task-form'); //The form at the top
 const filter = document.querySelector('#filter'); //the task filter text field
 const taskList = document.querySelector('.collection'); //The UL
 const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+
+
+// Add Event Listener  [Form , clearBtn and filter search input ]
+
+// form submit 
+form.addEventListener('submit', addNewTask);
+// Clear All Tasks
+clearBtn.addEventListener('click', clearAllTasks);
+//   Filter Task 
+filter.addEventListener('keyup', filterTasks);
