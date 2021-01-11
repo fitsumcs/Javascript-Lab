6f74b870b7da58309dbdfd54e7ebbc69c6607b90
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-11 17:23:12 +0300

    Adding Query selector to get a specific item from the DOM

---
+++ Lesson 02 [Lab 05]/Finished/assets/js/app.js
@@ -0,0 +1,6 @@
+// Define UI Variables 
+const taskInput = document.querySelector('#task'); //the task input text field
+const form = document.querySelector('#task-form'); //The form at the top
+const filter = document.querySelector('#filter'); //the task filter text field
+const taskList = document.querySelector('.collection'); //The UL
+const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
