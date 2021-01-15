45d1de7b0d25a65ac36d8675ca36a3b4f4fc353e
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-15 10:31:30 +0300

    Starting Lab 03

---
+++ Lesson 03 [Lab 06]/Finished/assets/js/bom.js
@@ -0,0 +1,30 @@
+/*  
+Here is the exercise on working on the remaining bom method 
+
+Location , Navigator , screen , Window 
+
+Follow the Instruction on the comments 
+
+1. Declare the UI Variables for selecting on the elements 
+2. Use the innerHTML property to display the result on each element 
+3. The Text  of the elements will lead you what bom information is required 
+
+Adding Extra is Possible if you want to explore more ...
+
+Good Luck !!! 
+*/
+
+
+
+
+// Define UI Variables  here 
+
+
+
+
+
+
+
+
+
+// Display the BOM Information on the innerHTML of the elements
