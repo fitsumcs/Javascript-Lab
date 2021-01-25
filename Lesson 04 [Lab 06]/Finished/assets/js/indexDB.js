3088c2d9c473ff5c76b3e699d245edf61cc4867c
Fitsum Alemu <fitsumcs@gmail.com>
2021-01-25 14:44:02 +0300

    Create Task Table with primery key of id of outoIncrement

--- Lesson 04 [Lab 06]/Finished/assets/js/indexDB.js
+++ Lesson 04 [Lab 06]/Finished/assets/js/indexDB.js
@@ -2,4 +2,24 @@
 var db;
 
 // create the database
-let openRequest = indexedDB.open("Tasks", 1);
+let openRequest = indexedDB.open("MyTasks", 1);
+
+//on Database successfully created
+openRequest.onsuccess = function(event) {
+    db = openRequest.result;
+    console.log('The database is opened successfully');
+
+};
+//on error occurred at opening the Database 
+openRequest.onerror = function(event) {
+    console.log('The database is opened failed');
+};
+//upgrade , this is called only once the db is created or version changed 
+openRequest.onupgradeneeded = function(event) {
+    db = event.target.result;
+    var objectStore;
+    if (!db.objectStoreNames.contains('tasks')) {
+        objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+    }
+
+}
