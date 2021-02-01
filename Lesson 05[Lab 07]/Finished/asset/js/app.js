557fa4614823afc1cc6e2e79a7bcf7e76deaaee8
Fitsum Alemu <fitsumcs@gmail.com>
2021-02-01 19:46:57 +0300

    Adding the loadPostFrom API

--- Lesson 05[Lab 07]/Finished/asset/js/app.js
+++ Lesson 05[Lab 07]/Finished/asset/js/app.js
@@ -3,4 +3,5 @@ document.addEventListener("DOMContentLoaded", () => {
     startTime();
     loadPost();
     loadPosts();
+    load_fromPlaceHolder();
 });
---
+++ Lesson 05[Lab 07]/Finished/asset/js/loadPostsAPI.js
@@ -0,0 +1,11 @@
+// UI Vars 
+const postDiv3 = document.getElementById('thePosts');
+
+
+
+//load a single customer function 
+function load_fromPlaceHolder() {
+
+
+
+}
