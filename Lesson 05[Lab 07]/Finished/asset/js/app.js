5fa26ad12b1bd29f1b12aca707761d4007f30495
Fitsum Alemu <fitsumcs@gmail.com>
2021-02-01 16:44:01 +0300

    Adding few comments

--- Lesson 05[Lab 07]/Finished/asset/js/app.js
+++ Lesson 05[Lab 07]/Finished/asset/js/app.js
@@ -26,16 +26,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //timer 
 function startTime() {
+    //retrieve date 
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
+    //get the AM / PM value 
     let am_pm = h > 12 ? 'PM' : 'AM';
+    // Convert the hour to 12 format 
     h = h % 12 || 12;
+    // add zero 
     m = addZero(m);
     s = addZero(s);
+    // Assign to the UI [p]
     timerDemo.innerHTML = `${h} : ${addZero(m)} : ${addZero(s)} ${am_pm }`;
-    var t = setTimeout(startTime, 500);
+    setTimeout(startTime, 500);
 }
 
 function addZero(i) {
