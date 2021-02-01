2bbdaedd48f80a578441329d179c955d0f3d0a35
Fitsum A <fitsum@gmail.com>
2021-02-01 21:42:53 +0300

    Converting to async/await

--- Lesson 05[Lab 07]/Finished/asset/js/loadPostsAPI.js
+++ Lesson 05[Lab 07]/Finished/asset/js/loadPostsAPI.js
@@ -3,7 +3,8 @@ const postDiv3 = document.getElementById('thePosts');
 
 //Load Every thing ....
 document.addEventListener("DOMContentLoaded", () => {
-    load_fromPlaceHolder();
+    //load_fromPlaceHolder();
+    loadDataNew();
 });
 
 
@@ -49,5 +50,52 @@ function load_fromPlaceHolder() {
         });
 
 
+
+}
+
+async function load_fromPlaceHolder_new() {
+
+    //open the request 
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    let data = await response.json();
+
+    return data;
+
+}
+
+function loadDataNew() {
+    load_fromPlaceHolder_new().then(function(posts) {
+            //iterate over each post [100 posts]
+            let output = '';
+            posts.forEach(function(post) {
+                output += `
+
+        <div class="item">
+        <div class="image">
+            <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+        </div>
+        <div class="content">
+            <a class="header" href="#" id="bTitle">
+            ${post.title.toUpperCase()}
+            </a>
+            <div class="description">
+                <p id="bDesc">
+                ${post.body}
+                </p>
+            </div>
+            <div class="extra">
+                <a class="ui floated basic violet button" href="#">Read Mores</a>
+            </div>
+        </div>
+    </div>
+
+`;
+            });
+            postDiv3.innerHTML = output;
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 
 }
