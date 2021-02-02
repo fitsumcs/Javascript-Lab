2fdb330481c825b54a3936c33436407542a69ed2
Fitsum Alemu <fitsumcs@gmail.com>
2021-02-02 11:05:28 +0300

    few minor updates

--- Lesson 05[Lab 07]/Finished/asset/js/timer.js
+++ Lesson 05[Lab 07]/Finished/asset/js/timer.js
@@ -20,6 +20,6 @@ function startTime() {
 }
 
 function addZero(i) {
-    if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
+    if (i < 10) { i = "0" + i } // add zero in front of numbers < 10
     return i;
 }
