ab8450f969cfbd46daa062f51714b5cd72b2b812
Fitsum Alemu <fitsumcs@gmail.com>
2021-02-10 15:33:07 +0300

    Taking the Li painting to UI class

--- Lesson 06 [Lab 08]/Finished/assets/js/app.js
+++ Lesson 06 [Lab 08]/Finished/assets/js/app.js
@@ -101,27 +101,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (cursor) {
 
-                // Create an li element when the user adds a task 
-                const li = document.createElement('li');
-                //add Attribute for delete 
-                li.setAttribute('data-task-id', cursor.value.id);
-                // Adding a class
-                li.className = 'collection-item';
-                // Create text node and append it 
-                li.appendChild(document.createTextNode(cursor.value.taskname));
-                // Create new element for the link 
-                const link = document.createElement('a');
-                // Add class and the x marker for a 
-                link.className = 'delete-item secondary-content';
-                link.innerHTML = `
-                 <i class="fa fa-remove"></i>
-                &nbsp;
-                <a href="/Lesson 04 [Lab 06]/Finished/edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>
-                `;
-                // Append link to li
-                li.appendChild(link);
-                // Append to UL 
-                taskList.appendChild(li);
+                addTaskListToUI(cursor);
+
                 cursor.continue();
             }
         }
